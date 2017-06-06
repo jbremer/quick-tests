b06f3fd245f47c6e029da570b604d539b557fafc
Jurriaan Bremer <jbr@cuckoo.sh>
2017-06-06 08:18:56 +0200

    basic structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2017 Jurriaan Bremer.
+
+import setuptools
+
+setuptools.setup(
+    name="QuickTests",
+    version="0.1",
+    author="Jurriaan Bremer",
+    author_email="jbr@cuckoo.sh",
+    packages=[
+        "qt",
+    ],
+    url="https://github.com/jbremer/quick-tests",
+    description="Quick CI Tests",
+    include_package_data=True,
+    install_requires=[
+    ],
+)
